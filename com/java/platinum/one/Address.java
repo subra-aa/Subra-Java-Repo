6e6f7570523ac3c0dadb76d0013460f7859d9aa6
Subra Talari <subratalari@subra.lan1>
2023-01-18 21:01:25 -0600

    Collections

---
+++ com/java/platinum/one/Address.java
@@ -0,0 +1,58 @@
+package com.java.platinum.one;
+
+public class Address {
+
+    private String addressLineOne;
+
+    private String addressLineTwo;
+
+    private String city;
+
+
+
+    private String state;
+
+    private String country;
+
+    public String getAddressLineOne() {
+        return addressLineOne;
+    }
+
+    public void setAddressLineOne(String addressLineOne) {
+        this.addressLineOne = addressLineOne;
+    }
+
+    public String getAddressLineTwo() {
+        return addressLineTwo;
+    }
+
+    public void setAddressLineTwo(String addressLineTwo) {
+        this.addressLineTwo = addressLineTwo;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+
+}
---
+++ com/java/platinum/one/Person.java
@@ -0,0 +1,50 @@
+package com.java.platinum.one;
+
+import java.util.List;
+
+public class Person {
+
+    private int rollNo;
+
+    private String name;
+
+
+
+    private Double marks;
+
+    private List<Address> addressList;
+
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getMarks() {
+        return marks;
+    }
+
+    public void setMarks(Double marks) {
+        this.marks = marks;
+    }
+
+    public List<Address> getAddressList() {
+        return addressList;
+    }
+
+    public void setAddressList(List<Address> addressList) {
+        this.addressList = addressList;
+    }
+
+}
